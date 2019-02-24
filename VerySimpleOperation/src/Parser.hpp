0f41e2c835eab5ffb3da364de46e02e82cb5fd37
KaceCottam <kace.cottam@wsu.edu>
2019-02-24 08:34:07 -0800

    implemented double and int parser.

--- VerySimpleOperation/src/Parser.hpp
+++ VerySimpleOperation/src/Parser.hpp
@@ -1,16 +1,20 @@
+//#comment file
 #pragma once
 #include <string>
 #include <unordered_map>
 #include <functional>
 #include <sstream>
 
+//#comment begin
 template<class T>
 class Parser
+//#comment end
 {
 public:
 	Parser() = default;
 	virtual ~Parser() = default;
 protected:
+	//#comment
 	struct pair_hash {
 		template <class T1, class T2>
 		std::size_t operator () (const std::pair<T1, T2> &p) const {
@@ -25,9 +29,11 @@ protected:
 	std::unordered_map<std::string, std::function<T(T&)>> UnaryOperators;
 	std::unordered_map<std::string, std::function<T(T&, T&)>> BinaryOperators;
 
-	//returns a pair of pairs, in the format {{left,right},{leftMostIndex,rightMostIndex}}
-	virtual std::pair<std::pair<T, T>, std::pair<int, int>> DiscoverInput(int index, std::string& in) = 0;
+	//#comment
+	virtual std::pair<std::pair<T, T>, std::pair<int, int>> DiscoverInput(int index, std::string& in, std::string& operation) = 0;
+	//#comment
 	virtual int CheckFunction(std::string& in) = 0;
+	//#comment
 	bool DoParse(std::string& inputString, T& output, int const steps = Infinite)
 	{
 		switch (steps)
@@ -46,63 +52,62 @@ protected:
 		}
 		case End:
 		{
-			if (WrappingOperators.size() != 0)
-				for (auto& i : WrappingOperators)
+			for (auto& i : WrappingOperators)
+			{
+				auto key = i.first;
+				auto evaluate = i.second;
+				//Find Wrapping Operators
+				int positionOfWrappingOperatorBegin = inputString.find_last_of(key.first);
+				if (positionOfWrappingOperatorBegin != std::string::npos)
 				{
-					auto key = i.first;
-					auto evaluate = i.second;
-					//Find Wrapping Operators
-					int positionOfWrappingOperatorBegin = inputString.find_last_of(key.first);
-					if (positionOfWrappingOperatorBegin != std::string::npos)
+					auto trimString = [](std::string& input)
 					{
-						auto trimString = [](std::string& input)
-						{
-							input = input.c_str();
-							return input;
-						};
-						auto min = [](T const& num, T const& min)
-						{
-							return (int)(num < min ? min : num);
-						};
-						auto max = [](T const&num, T const& max)
-						{
-							return (int)(num > max ? max : num);
-						};
+						input = input.c_str();
+						return input;
+					};
+					auto min = [](T const& num, T const& min)
+					{
+						return (int)(num < min ? min : num);
+					};
+					auto max = [](T const&num, T const& max)
+					{
+						return (int)(num > max ? max : num);
+					};
 
-						auto inputCopy = std::string(&(inputString[positionOfWrappingOperatorBegin + 1]));
-						int positionofWrappingOperatorEnd = inputCopy.find_first_of(key.second);
-						if(positionofWrappingOperatorEnd == std::string::npos)
-						{
-							throw "Broke!";
-						}
-						inputCopy[positionofWrappingOperatorEnd] = '\0';
-						trimString(inputCopy);
-						int lengthOfReplacementString = max(positionofWrappingOperatorEnd+1, inputString.length());
-						std::string beforeEvaluate = inputString.substr(
-							min(positionOfWrappingOperatorBegin, 0),
-							lengthOfReplacementString + 1
-						);
-						auto evaluationResult = evaluate(inputCopy,output);
-
-						auto findResult = inputString.find(beforeEvaluate);
-
-						if (findResult == std::string::npos)
-						{
-							throw std::out_of_range("Wrapping broke!");
-						}
-
-						if (evaluationResult == Finished)
-						{
-							inputString.replace(positionOfWrappingOperatorBegin, lengthOfReplacementString+1, inputCopy, 0);
-						}
-						else
-						{
-						inputString.replace(positionOfWrappingOperatorBegin, lengthOfReplacementString+1, inputCopy, 0);
-						}
-
-						return Working;
+					auto inputCopy = std::string(&(inputString[positionOfWrappingOperatorBegin + 1]));
+					int positionofWrappingOperatorEnd = inputCopy.find_first_of(key.second);
+					if (positionofWrappingOperatorEnd == std::string::npos)
+					{
+						throw "Broke!";
 					}
+					inputCopy[positionofWrappingOperatorEnd] = '\0';
+					trimString(inputCopy);
+					int lengthOfReplacementString = max(positionofWrappingOperatorEnd + 1, inputString.length());
+					std::string beforeEvaluate = inputString.substr(
+						min(positionOfWrappingOperatorBegin, 0),
+						lengthOfReplacementString + 1
+					);
+					auto evaluationResult = evaluate(inputCopy, output);
+
+					auto findResult = inputString.find(beforeEvaluate);
+
+					if (findResult == std::string::npos)
+					{
+						throw std::out_of_range("Wrapping broke!");
+					}
+
+					if (evaluationResult == Finished)
+					{
+						inputString.replace(positionOfWrappingOperatorBegin, lengthOfReplacementString + 1, inputCopy, 0);
+					}
+					else
+					{
+						inputString.replace(positionOfWrappingOperatorBegin, lengthOfReplacementString + 1, inputCopy, 0);
+					}
+
+					return Working;
 				}
+			}
 
 			//if none, find operators (unary, then binary)
 			auto discover = [](std::string& in, std::string& srch)
@@ -129,15 +134,15 @@ protected:
 				auto operatorString = i.first;
 				auto operatorFunction = i.second;
 				auto discoverIndex = discover(inputString, operatorString);
-				if (discoverIndex == std::string::npos)
+				if (discoverIndex != std::string::npos)
 				{
 					discovery.second.first = Unary;
 					discovery.second.second = operatorString;
-					discovery.first = DiscoverInput(discoverIndex, inputString);
+					discovery.first = DiscoverInput(discoverIndex, inputString, operatorString);
 				}
 			}
 
-			for (auto& i: BinaryOperators)
+			for (auto& i : BinaryOperators)
 			{
 				auto operatorString = i.first;
 				auto operatorFunction = i.second;
@@ -146,7 +151,7 @@ protected:
 				{
 					discovery.second.first = Binary;
 					discovery.second.second = operatorString;
-					discovery.first = DiscoverInput(discoverIndex, inputString);
+					discovery.first = DiscoverInput(discoverIndex, inputString, operatorString);
 					break;
 				}
 			}
@@ -169,7 +174,7 @@ protected:
 				}
 				std::ostringstream outputString;
 				outputString << result;
-				inputString.replace(coords.first, coords.second - coords.first+1, outputString.str(), 0);
+				inputString.replace(coords.first, coords.second - coords.first + 1, outputString.str(), 0);
 
 				return Working;
 			}
@@ -181,17 +186,19 @@ protected:
 		}
 	}
 public:
+	//#comment
 	int RegisterUnaryOperator(std::string const& inputs, std::function<T(T&)> const function)
 	{
 		UnaryOperators[std::string(inputs)] = function;
 		return 1;
 	}
+	//#comment
 	int RegisterBinaryOperator(std::string const& inputs, std::function<T(T&, T&)> const function)
 	{
 		BinaryOperators[std::string(inputs)] = function;
 		return 1;
 	}
-
+	//#comment
 	int RegisterWrappingOperator(std::string const& left, std::string const& right, std::function<bool(std::string&, T&)> const function)
 	{
 		WrappingOperators[{left, right}] = function;
@@ -208,8 +215,10 @@ public:
 		Working = false,
 		Finished = true
 	};
+	//#comment begin
 	virtual bool Parse(std::string& inputString, T& output, int const steps = Infinite)
 	{
-		return DoParse(inputString, output, steps); //Add check functions
+		return DoParse(inputString, output, steps);
 	}
+	//#comment end
 };
